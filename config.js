ef458cd43d4193b68c86567820cea9ebbd463353
maria-rangwala <maria.rangwala.16@gmail.com>
2025-05-11 13:32:29 +0530

    first

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT =process.env.PORT || 5555;
+
+export const mongoDBURL = 'mongodb+srv://mariar786110:maria242703@book-store-mern.coagfkp.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Book-store-MERN';
